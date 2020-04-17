75e4cbf717b232119b0e8c396cac0a2032b8d70d
Dennis Frey <dennismfrey@gmail.com>
2020-04-17 13:44:14 -0400

    initial spring project

---
+++ src/main/java/com/greeting/GreetingWebApplication.java
@@ -0,0 +1,13 @@
+package com.greeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingWebApplication.class, args);
+	}
+
+}
